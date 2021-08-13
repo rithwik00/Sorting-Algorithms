5759242221dd7a2de1cce0e6024b59a4f736456d
Rithwik <vedpathakrithwik@gmail.com>
2021-08-13 23:51:09 +0530

    n removed

--- HeapSort.java
+++ HeapSort.java
@@ -51,7 +51,7 @@ class HeapSort {
  
     public static void main(String args[]) {
         int arr[] = { 12, 11, 13, 5, 6, 7 };
-        int n = arr.length;
+        // int n = arr.length;
  
         HeapSort ob = new HeapSort();
         ob.sort(arr);
